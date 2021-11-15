760763294889f1c3ec34c39efc0f670bcf071ced
nahodkins <tsuber2001@gmail.com>
2021-11-16 00:37:18 +0200

    Revert "Adds 1st task implementation"
    This reverts commit 01fef09328af353be22c9ea16de86f07a8fb7b72.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        Person adam = new Person("Adam", 20, true);
-        Person john = new Person("John", 13, false);
-
-        System.out.println(adam);
-        System.out.println(john);
+        System.out.println("Hello World!");
     }
 }
