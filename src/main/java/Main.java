37545c8134a7112586ad42953cc762e52aef3e6e
nahodkins <tsuber2001@gmail.com>
2021-11-16 00:38:03 +0200

    Adds 1st task implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Person adam = new Person("Adam", 20, true);
+        Person john = new Person("John", 13, false);
+
+        System.out.println(adam);
+        System.out.println(john);
     }
 }
