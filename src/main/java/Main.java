a43120b651160e42e6319ec3f96bbd82db5fbf0d
nahodkins <tsuber2001@gmail.com>
2021-11-16 17:17:25 +0200

    Adds 6th task implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,26 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        List<MyInterface> implementations = Arrays.asList(
+                () -> System.out.println("First implementation"),
+                new MyInterface() {
+                    @Override
+                    public void doSomeStaff() {
+                        System.out.println("Second implementation");
+                    }
+                },
+                () -> System.out.println("Third implementation")
+        );
+        implementations.forEach(MyInterface::doSomeStaff);
+
+        new MyAbstractClass(){
+            @Override
+            public void sayHello() {
+                System.out.println("Hello");
+            }
+        }.sayHello();
     }
 }
---
+++ src/main/java/MyAbstractClass.java
@@ -0,0 +1,4 @@
+public abstract class MyAbstractClass {
+
+    public abstract void sayHello();
+}
