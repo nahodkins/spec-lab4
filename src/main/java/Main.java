ce099005bb7a786c5fb404beec4bab089e0daad0
nahodkins <tsuber2001@gmail.com>
2021-11-18 23:47:30 +0200

    Adds 1st task implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,5 +6,8 @@ public class Main {
 
         System.out.println(adam);
         System.out.println(john);
+
+        adam.setAge(21);
+        System.out.println("Adam's age: " + adam.getAge());
     }
 }
