a6b1195fb5fba982a85c865eafed60bc3b1a506f
nahodkins <tsuber2001@gmail.com>
2021-11-18 22:58:08 +0200

    Adds 5th task implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,8 +8,9 @@ public class Main {
         XorIntegerBlockEncryption encryption = new XorIntegerBlockEncryption();
         Integer[] given = {1, 2, 3, 4, 5, 6, 7};
         Integer[] encrypted = encryption.encryptData(given);
-        System.out.println("Encrypted: " + Arrays.toString(encrypted));
         Integer[] decrypted = encryption.decryptData(encrypted);
+
+        System.out.println("Encrypted: " + Arrays.toString(encrypted));
         System.out.println("Decrypted:" + Arrays.toString(decrypted));
         System.out.println("Is decryption correct: " + Arrays.equals(given, decrypted));
     }
